87788c7b35c20094ee4c33b63be5c6a7feebe66b
diogo-es <101022369+diogo-es@users.noreply.github.com>
2025-02-08 13:46:00 +0000

    configuração dos toasts

--- src/main.js
+++ src/main.js
@@ -1,11 +1,29 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
+import Toast from "vue-toastification"
+import "vue-toastification/dist/index.css"
 import { createPinia } from 'pinia'
 import router from './router';
 
 
 const app = createApp(App);
 
+// Default/Global Toast configuration
+app.use(Toast, {
+    position: "top-center",
+    timeout: 3000,
+    closeOnClick: true,
+    pauseOnFocusLoss: true,
+    pauseOnHover: true,
+    draggable: true,
+    draggablePercent: 0.6,
+    showCloseButtonOnHover: true,
+    hideProgressBar: true,
+    closeButton: "button",
+    icon: true,
+    rtl: false
+})
 app.use(createPinia())
 app.use(router);
 
